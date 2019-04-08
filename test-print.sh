6bc664b1448d9e59b68d5132b17d6d276aca093d
WenqiJiang <wenqi.j@columbia.edu>
2019-04-08 14:05:44 -0400

    link opencv

--- test-print.sh
+++ test-print.sh
@@ -1,4 +1,7 @@
 ./microc.native micro-test/test-print.im > comp.ll
 gcc -c built-in.c
-clang -o a.out built-in.o comp.ll
+g++ -c img_io.cpp
+g++ edge_detection.cpp -o output `pkg-config --cflags --libs opencv`
+clang++ `pkg-config --cflags opencv` `pkg-config --libs opencv` img_io.cpp built-in.o comp.ll -o main
+clang -o a.out built-in.o img_io.o comp.ll
 ./a.out
