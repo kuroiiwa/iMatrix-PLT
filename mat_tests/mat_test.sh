9cc1f4932a0a003390d6d9a6f16e76a1731cb6d3
zzaustc <zzaustc@163.com>
2019-05-16 18:52:01 -0400

    assemble tests

--- mat_tests/mat_test.sh
+++
@@ -1,5 +0,0 @@
-make -C ./lib/
-../imatrix ./test-eigen_value.im > tmp.ll
-clang++ -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs -o a.out tmp.ll ../lib/lib.a
-./a.out > ./test-eigen_value.out
-
